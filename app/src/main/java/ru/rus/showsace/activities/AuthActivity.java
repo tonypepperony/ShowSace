977feee98197b8afc9d8999167ff4636620c5381
Ruslan <rus.official@mail.ru>
2016-11-13 13:43:14 +0300

    step 2

--- app/src/main/java/ru/rus/showsace/activities/AuthActivity.java
+++ app/src/main/java/ru/rus/showsace/activities/AuthActivity.java
@@ -4,12 +4,32 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import ru.rus.showsace.R;
+import ru.rus.showsace.fragments.LoginFragment;
 
-public class AuthActivity extends AppCompatActivity {
+public class AuthActivity extends AppCompatActivity implements LoginFragment.OnFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth);
+
+        getSupportFragmentManager().beginTransaction().add(R.id.activity_auth, LoginFragment.newInstance())
+                .addToBackStack("login fragment")
+                .commit();
+    }
+
+    @Override
+    public void onLoginAttempt(String login, String password) {
+        //TODO: add logic
+    }
+
+    @Override
+    public void onRegistrationRequested() {
+        //TODO: add logic
+    }
+
+    @Override
+    public void onRestoreRequested() {
+        //TODO: add logic
     }
 }
