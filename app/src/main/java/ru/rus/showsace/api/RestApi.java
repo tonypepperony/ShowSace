166a45a44fa17c438efc80f0f408bf8d107c43fa
Ruslan <rus.official@mail.ru>
2016-11-13 13:50:18 +0300

    step 3 +RestApi

---
+++ app/src/main/java/ru/rus/showsace/api/RestApi.java
@@ -0,0 +1,34 @@
+package ru.rus.showsace.api;
+
+
+import android.os.Handler;
+
+public class RestApi {
+    public static void login(String login, String password) {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //TODO
+            }
+        }, 3000);
+    }
+
+    public static void register(String name, String login, String password){
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //TODO
+            }
+        }, 3000);
+    }
+
+    public static void restore(String login){
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //TODO
+            }
+        }, 3000);
+    }
+
+}
