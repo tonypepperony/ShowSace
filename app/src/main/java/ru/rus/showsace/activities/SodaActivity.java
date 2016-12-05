ac97d2bef669810333036edfb99b9a6ca0f8789e
Ruslan <rus.official@mail.ru>
2016-12-06 00:48:16 +0300

    step 14 add sodas

--- app/src/main/java/ru/rus/showsace/activities/SodaActivity.java
+++ app/src/main/java/ru/rus/showsace/activities/SodaActivity.java
@@ -24,6 +24,14 @@ public class SodaActivity extends AppCompatActivity {
         items.add(new Item(001, "Fanta Peach", R.drawable.fantapeach));
         items.add(new Item(002, "Fanta Berry", R.drawable.fantaberry));
         items.add(new Item(003, "Fanta Grape", R.drawable.fantagrape));
+        items.add(new Item(004, "Mtn DEW Baja Blast", R.drawable.soda01));
+        items.add(new Item(005, "Mtn DEW Voltage", R.drawable.soda02));
+        items.add(new Item(006, "Fanta Mango", R.drawable.soda03));
+        items.add(new Item(007, "Coca-Cola Cherry", R.drawable.soda04));
+        items.add(new Item(800, "Fanta Apple", R.drawable.soda05));
+        items.add(new Item(900, "Fanta Cherry", R.drawable.soda06));
+        items.add(new Item(010, "Fanta Pineapple", R.drawable.soda07));
+        items.add(new Item(011, "Fanta Strawberry", R.drawable.soda08));
 
         itemsAdapter.setItems(items);
     }
