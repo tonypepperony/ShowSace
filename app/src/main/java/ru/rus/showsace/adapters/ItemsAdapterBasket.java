6d30b34b181d5d6e7ef64d994996ba72e0f704cc
Ruslan <rus.official@mail.ru>
2016-12-11 19:05:55 +0300

    step 21

---
+++ app/src/main/java/ru/rus/showsace/adapters/ItemsAdapterBasket.java
@@ -0,0 +1,95 @@
+package ru.rus.showsace.adapters;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import ru.rus.showsace.R;
+import ru.rus.showsace.models.Item;
+
+/**
+ * Created by Ruslan on 05.12.16.
+ */
+
+public class ItemsAdapterBasket extends BaseAdapter {
+ArrayList<Item> items = new ArrayList<>();
+
+    public void setItems(ArrayList<Item> items) {
+        this.items = items;
+        notifyDataSetChanged(); //рефреш списка
+    }
+
+    @Override
+    public int getCount() {
+        return items.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return items.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        final ViewHolder holder;
+        LayoutInflater inflater = LayoutInflater.from(viewGroup.getContext());
+        if (view == null){ //если итем не создан, создаем, если создан заменяем
+            holder = new ViewHolder();
+
+            view = inflater.inflate(R.layout.listview_items_tovary, viewGroup, false);
+
+            holder.imageView = (ImageView) view.findViewById(R.id.image60);
+            holder.textView = (TextView) view.findViewById(R.id.text60);
+            holder.addButton = (Button) view.findViewById(R.id.addButton);
+            holder.removeButton = (Button) view.findViewById(R.id.removeButton);
+            holder.priceView = (TextView) view.findViewById(R.id.textPrice60);
+
+            view.setTag(holder);
+            Log.i("TAG", "Inflate");
+        } else {
+            holder = (ViewHolder) view.getTag();
+            Log.i("TAG", "Inflate");
+        }
+
+        final Item item = items.get(i);
+        holder.imageView.setImageResource(item.getResId());
+        holder.textView.setText(item.getName());
+        holder.addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO: обработать кнопку
+            }
+        });
+        holder.removeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO: обработать кнопку
+            }
+        });
+
+        holder.priceView.setText(""+item.getPrice()+"p.");
+
+
+        return view;
+    }
+
+    private static class ViewHolder{
+        public ImageView imageView;
+        public TextView textView;
+        public Button addButton;
+        public Button removeButton;
+        public TextView priceView;
+    }
+}
