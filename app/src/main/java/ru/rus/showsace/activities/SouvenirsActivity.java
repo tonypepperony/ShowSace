be581763236000ce7d430b39587ad845f2f4870c
Ruslan <rus.official@mail.ru>
2016-12-11 16:56:45 +0300

    step 16 add stickers and souvs

--- app/src/main/java/ru/rus/showsace/activities/SouvenirsActivity.java
+++ app/src/main/java/ru/rus/showsace/activities/SouvenirsActivity.java
@@ -2,8 +2,13 @@ package ru.rus.showsace.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 import ru.rus.showsace.R;
+import ru.rus.showsace.adapters.ItemsAdapter;
+import ru.rus.showsace.models.Item;
 
 public class SouvenirsActivity extends AppCompatActivity {
 
@@ -11,5 +16,18 @@ public class SouvenirsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_souvenirs);
+        ListView listView = (ListView) findViewById(R.id.listItemsSouvenirs);
+
+        ItemsAdapter itemsAdapter = new ItemsAdapter();
+        listView.setAdapter(itemsAdapter);
+
+        ArrayList<Item> items = new ArrayList<>();
+        items.add(new Item(301, "Darth Vader", R.drawable.souv01));
+        items.add(new Item(302, "Batman", R.drawable.souv02));
+        items.add(new Item(303, "Casio f-91w white", R.drawable.souv03));
+        items.add(new Item(304, "Casio f-91w black", R.drawable.souv04));
+        items.add(new Item(305, "Casio f-91w metallic", R.drawable.souv05));
+
+        itemsAdapter.setItems(items);
     }
 }
--- app/src/main/java/ru/rus/showsace/activities/StickersActivity.java
+++ app/src/main/java/ru/rus/showsace/activities/StickersActivity.java
@@ -2,8 +2,13 @@ package ru.rus.showsace.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 import ru.rus.showsace.R;
+import ru.rus.showsace.adapters.ItemsAdapter;
+import ru.rus.showsace.models.Item;
 
 public class StickersActivity extends AppCompatActivity {
 
@@ -11,5 +16,19 @@ public class StickersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stickers);
+        ListView listView = (ListView) findViewById(R.id.listItemsStickers);
+
+        ItemsAdapter itemsAdapter = new ItemsAdapter();
+        listView.setAdapter(itemsAdapter);
+
+        ArrayList<Item> items = new ArrayList<>();
+        items.add(new Item(201, "Sticker pack #1", R.drawable.stick01));
+        items.add(new Item(202, "Sticker pack #2", R.drawable.stick02));
+        items.add(new Item(203, "Sticker pack #3", R.drawable.stick03));
+        items.add(new Item(204, "Sticker pack #4", R.drawable.stick04));
+        items.add(new Item(205, "Sticker pack #5", R.drawable.stick05));
+        items.add(new Item(206, "Sticker pack #6", R.drawable.stick06));
+
+        itemsAdapter.setItems(items);
     }
 }
