fce3ed2f65f039367041b276f5425a060839daca
Ruslan <rus.official@mail.ru>
2016-12-13 00:19:25 +0300

    step 33 add clear button

--- app/src/main/java/ru/rus/showsace/activities/BasketActivity.java
+++ app/src/main/java/ru/rus/showsace/activities/BasketActivity.java
@@ -2,6 +2,8 @@ package ru.rus.showsace.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -25,5 +27,22 @@ public class BasketActivity extends AppCompatActivity {
         ArrayList<Item> items = new ArrayList<>(App.getZakaz());
 
         itemsAdapter.setItems(items);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.mainbasket, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.clear) {
+            App.getInstance().clearZakaz();
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
