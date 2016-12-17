8c77f8fa1ed23ec7a235d627d79a82d057e52a97
Ruslan <rus.official@mail.ru>
2016-12-17 11:25:13 +0300

    step 45 add finish

--- app/src/main/java/ru/rus/showsace/activities/FinishActivity.java
+++ app/src/main/java/ru/rus/showsace/activities/FinishActivity.java
@@ -1,15 +1,28 @@
 package ru.rus.showsace.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import ru.rus.showsace.R;
 
 public class FinishActivity extends AppCompatActivity {
+    Button finishBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_finish);
+
+        finishBtn = (Button) findViewById(R.id.finishBtn);
+        finishBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getBaseContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
