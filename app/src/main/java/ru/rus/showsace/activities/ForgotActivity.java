6109a548c23af52fe35e0a4a0f36861f4cb75f3a
Ruslan <rus.official@mail.ru>
2016-12-20 22:51:41 +0300

    step 63 forgot password

--- app/src/main/java/ru/rus/showsace/activities/ForgotActivity.java
+++ app/src/main/java/ru/rus/showsace/activities/ForgotActivity.java
@@ -1,7 +1,12 @@
 package ru.rus.showsace.activities;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import ru.rus.showsace.R;
 
@@ -11,5 +16,26 @@ public class ForgotActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot);
+
+        final EditText etEmail = (EditText) findViewById(R.id.editTextForgot);
+
+        Button sendBtn = (Button) findViewById(R.id.buttonSendPass);
+        sendBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (etEmail.getText().length() < 5){
+                    Toast.makeText(getBaseContext(), "введите email", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getBaseContext(), "пароль выслан", Toast.LENGTH_SHORT).show();
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            finish();
+                        }
+                    }, 2000);
+                }
+
+            }
+        });
     }
 }
